0c2b02cf279521724e5440f9468346ef992363e3
efominykh <efominykh@neoflex.ru>
2025-11-28 07:28:35 +0300

    387

---
+++ src/problems/queue/easy/Solution_387_First_Unique_Char_In_String.java
@@ -0,0 +1,26 @@
+package problems.queue.easy;
+
+
+import java.util.*;
+
+public class Solution_387_First_Unique_Char_In_String {
+
+    public static int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            freq[c - 'a']++;
+        }
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (freq[c - 'a'] == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(firstUniqChar("loveleetcode"));
+    }
+}
